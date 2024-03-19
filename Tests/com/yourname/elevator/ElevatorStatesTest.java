7527f6e25461a27da633214b7b7427063f687b51
Dicenas <146039837+Dicenas@users.noreply.github.com>
2024-03-19 09:01:21 -0400

    Changed name for Elevator state transition test class

--- Tests/com/yourname/elevator/ElevatorStatesTest.java
+++ Tests/com/yourname/elevator/ElevatorStatesTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class ElevatorSimulationTest {
+class ElevatorStatesTest {
 
     private Elevator elevator;
 
